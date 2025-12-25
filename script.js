1b34e6206c15843ff6ec8a151726b5741e21292f
remyjdavis <remyjdavis90@gmail.com>
2025-12-25 13:27:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API =
-  "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+  "https://script.google.com/macros/s/AKfycbzB0d5yltjq5Y1kmk9jDmrgUpRw9NnozKctgh0ELGb6cde7I51xqbcXDoUBbPDjygI5/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
   fetch(`${API}?action=tanks`)
