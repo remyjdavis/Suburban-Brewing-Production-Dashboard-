f7ae363b26d7a3d5049cadde51f67003e84c69fc
remyjdavis <remyjdavis90@gmail.com>
2025-12-25 14:26:42 -0500

    Update brew-log.js

--- brew-log.js
+++ brew-log.js
@@ -1,5 +1,6 @@
 const params = new URLSearchParams(location.search);
 const tank = params.get("tank");
+
 document.getElementById("tank").value = tank;
 document.getElementById("date").valueAsDate = new Date();
 
@@ -10,13 +11,32 @@ function saveBrew() {
     beer: beer.value,
     brewer: brewer.value,
     date: date.value,
+    targets: {
+      targetOG: targetOG.value,
+      actualOG: actualOG.value,
+      targetVol: targetVol.value,
+      actualVol: actualVol.value,
+      efficiency: efficiency.value,
+      yeast: yeast.value,
+      pitchRate: pitchRate.value
+    },
+    grainBill: grainBill.value,
+    water: {
+      ca: ca.value,
+      mg: mg.value,
+      na: na.value,
+      so4: so4.value,
+      cl: cl.value,
+      hco3: hco3.value
+    },
     mash: {
       pH: mashPh.value,
       notes: mashNotes.value
     },
+    hopSchedule: hopSchedule.value,
     lauter: {
-      first: firstRun.value,
-      final: finalRun.value,
+      firstRun: firstRun.value,
+      finalRun: finalRun.value,
       notes: lauterNotes.value
     },
     boil: {
@@ -32,8 +52,11 @@ function saveBrew() {
     checkedBy: checkedBy.value
   };
 
-  fetch("PASTE_YOUR_SCRIPT_URL_HERE", {
-    method: "POST",
-    body: JSON.stringify(payload)
-  }).then(() => alert("Brew Log Saved"));
+  fetch(
+    "https://script.google.com/macros/s/AKfycbzB0d5yltjq5Y1kmk9jDmrgUpRw9NnozKctgh0ELGb6cde7I51xqbcXDoUBbPDjygI5/exec",
+    {
+      method: "POST",
+      body: JSON.stringify(payload)
+    }
+  ).then(() => alert("Brew Log Saved"));
 }
